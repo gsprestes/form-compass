7abec12f77416553269837c131e0184a4ad1ee10
gsprestes~ <guilherme26prestes@gmail.com>
2022-06-06 00:19:52 -0300

    Feat: added validation of inputs.

--- scripts/main.js
+++ scripts/main.js
@@ -29,22 +29,60 @@ const isValid = () => {
     const password = document.querySelector('#password');
     const birthday = document.querySelector('#birthday');
     const terms = document.querySelector('#terms');
+    let validation = true;
 
-    if(name.value.length<4 || name.value.count(" ")) {
-        const p = document.createElement('p');
-        p.classList.add("error");
-        p.innerHTML = "Fullname Invalid";
-        name.after(p)
-        return false;
+    if(name.value.length<4 || !name.value.match(/ /g).length) {
+        const p = document.querySelector('.form-name .error');
+        p.setAttribute('style', 'visibility:visible');
+        validation = false;
     }
 
-    return true;
+    if (!/^[a-z0-9]+([\.-]?[a-z0-9]+)*@[a-z0-9]+([\.-]?[a-z0-9]+)*(\.[a-z]{2,3})+$/.test(email.value)) {
+        const p = document.querySelector('.email .error');
+        p.setAttribute('style', 'visibility:visible');
+        validation = false;
+    }
+
+    if (phone.value.length < 11 || !/\d+/.test(phone.value)) {
+        const p = document.querySelector('.phone .error');
+        p.setAttribute('style', 'visibility:visible');
+        validation = false;
+    }
+
+    if (password.value.length < 6 || password.value.length >= 9 || !/\d+/.test(password.value)) {
+        const p = document.querySelector('.pass .error');
+        p.setAttribute('style', 'visibility:visible');
+        validation = false;
+    }
+
+    if(birthday.value){
+        const birthdate = new Date(birthday.value);
+        const today = new Date();
+        const diff = today.getTime() - birthdate.getTime();
+        const age = Math.floor(diff/(1000*60*60*24*365.25));
+        if (age < 18 || age > 70) {
+            const p = document.querySelector('.birthday .error');
+            p.setAttribute('style', 'visibility:visible');
+            validation = false;
+        }
+    }else{
+        const p = document.querySelector('.birthday .error');
+        p.setAttribute('style', 'visibility:visible');
+        validation = false;
+    }
+
+    if (!terms.checked){
+        const p = document.querySelector('.terms .error');
+            p.setAttribute('style', 'visibility:visible');
+            validation = false;
+    }
+    return validation;
 }
 
 const submit = async () => {
     const errors = document.querySelectorAll(".error");
     errors.forEach((error) => {
-        error.remove();
+        error.setAttribute('style', 'visibility:hidden');
     })
     if (isValid()) {
        await navigation("/sent")
@@ -60,3 +98,7 @@ const goForm = async () => {
 document.addEventListener('DOMContentLoaded', function () {
     goForm();
 }, false);
+
+const goBack = () => {
+    // to do later
+}
